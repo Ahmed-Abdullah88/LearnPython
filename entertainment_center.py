621bc43d363503674e05efc5e5069cfb62473996
Ahmed-Abdullah88 <ahmedbaseer111@gmail.com>
2018-08-01 16:51:09 +0200

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,1 +1,41 @@
+import media
+import fresh_tomatoes
 
+
+print('START:\n\n')
+
+
+
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", 
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", 
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+
+avatar = media.Movie("Avatar", "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+school_of_rock = media.Movie("School of Rock", "Using rock music to learn",
+                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille", "A rat is a chef if Paris",
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+
+midnight_in_paris = media.Movie("Midnight in Paris", "Going back in time to meet authors",
+                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
+
+hunger_games = media.Movie("Hunger Games", "A really real reality show",
+                           "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                           "https://www.youtube.com/watch?v=4S9a5V9ODuY")
+
+movies = []
+
+
+#  fresh_tomatoes.open_movies_page()
+
+print('\nDONE!')
